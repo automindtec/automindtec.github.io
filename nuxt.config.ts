d38a7ba181349043b25d23f2ed8b0fd97e9a09f3
xilian <xilian@pixl.world>
2024-11-17 15:13:39 +0800

    K

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,5 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/automindtec.github.io/' // 使用你的 Repo 名稱作為 baseURL
-  }, 
-
   compatibilityDate: '2024-11-17',
 })
