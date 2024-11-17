1c3596aa466d60f3f9449b5da514f902e5029112
xilian <xilian@pixl.world>
2024-11-17 14:56:52 +0800

    update folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
   app: {
-    baseURL: '/automindweb/' // 使用你的 Repo 名稱作為 baseURL
+    baseURL: '/automindtec.github.io/' // 使用你的 Repo 名稱作為 baseURL
   }, 
 
   compatibilityDate: '2024-11-17',
